926b5af1f8459bbff245d1a23e9c8ffd59bad7f6
heartsentwined <heartsentwined.me@gmail.com>
2013-10-10 12:40:10 +0800

    built dist files

--- dist/ember-auth-session-cookie.js
+++ dist/ember-auth-session-cookie.js
@@ -96,7 +96,7 @@ var get$ = Ember.get;
 Em.onLoad('Ember.Application', function (application) {
   return application.initializer({
     name: 'ember-auth.session.cookie',
-    after: 'ember-auth',
+    before: 'ember-auth-load',
     initialize: function (container, app) {
       return app.register('authSession:cookie', get$(get$(Em, 'Auth'), 'CookieAuthSession'));
     }
